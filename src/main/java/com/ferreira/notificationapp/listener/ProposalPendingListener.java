5699d6241080da8988f261e10c2d18115cbedecb
Cristiane Ferreira <cris.mayara@gmail.com>
2024-10-25 13:31:52 -0300

    fix: resolve convert notification message

--- src/main/java/com/ferreira/notificationapp/listener/ProposalPendingListener.java
+++ src/main/java/com/ferreira/notificationapp/listener/ProposalPendingListener.java
@@ -2,14 +2,18 @@ package com.ferreira.notificationapp.listener;
 
 import com.ferreira.notificationapp.constant.MessageConstant;
 import com.ferreira.notificationapp.domain.Proposal;
+import com.ferreira.notificationapp.service.NotificationSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProposalPendingListener {
 
+    private NotificationSnsService notificationSnsService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposal.pending}")
     public void proposalPending(Proposal proposal) {
         String message = String.format(MessageConstant.PROPOSAL_UNDER_ANALYSIS, proposal.getUser().getFirstName());
+        notificationSnsService.sendNotification(message);
     }
 }
---
+++ src/main/java/com/ferreira/notificationapp/service/NotificationSnsService.java
@@ -0,0 +1,11 @@
+package com.ferreira.notificationapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationSnsService {
+
+    public void sendNotification(String message) {
+
+    }
+}
