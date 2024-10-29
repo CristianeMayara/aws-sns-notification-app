8b9437074ec7c5c78bb81718f5611a7ac092bd88
Cristiane Ferreira <cris.mayara@gmail.com>
2024-10-28 21:51:25 -0300

    feat: send notification to aws sns

--- src/main/java/com/ferreira/notificationapp/listener/ProposalPendingListener.java
+++ src/main/java/com/ferreira/notificationapp/listener/ProposalPendingListener.java
@@ -4,16 +4,18 @@ import com.ferreira.notificationapp.constant.MessageConstant;
 import com.ferreira.notificationapp.domain.Proposal;
 import com.ferreira.notificationapp.service.NotificationSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProposalPendingListener {
 
+    @Autowired
     private NotificationSnsService notificationSnsService;
 
     @RabbitListener(queues = "${rabbitmq.queue.proposal.pending}")
     public void proposalPending(Proposal proposal) {
         String message = String.format(MessageConstant.PROPOSAL_UNDER_ANALYSIS, proposal.getUser().getFirstName());
-        notificationSnsService.sendNotification(message);
+        notificationSnsService.sendNotification(proposal.getUser().getPhone(), message);
     }
 }
--- src/main/java/com/ferreira/notificationapp/service/NotificationSnsService.java
+++ src/main/java/com/ferreira/notificationapp/service/NotificationSnsService.java
@@ -1,11 +1,18 @@
 package com.ferreira.notificationapp.service;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationSnsService {
 
-    public void sendNotification(String message) {
+    @Autowired
+    private AmazonSNS amazonSNS;
 
+    public void sendNotification(String phone, String message) {
+        PublishRequest publishRequest = new PublishRequest().withMessage(message).withPhoneNumber(phone);
+        amazonSNS.publish(publishRequest);
     }
 }
